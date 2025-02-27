b38b1290eb77cdabecc017003791bbcaf1aca750
goksinb <gbasok@gmail.com>
2025-02-27 22:15:12 +0100

    add css

--- src/components/getRecipes.jsx
+++ src/components/getRecipes.jsx
@@ -1,6 +1,6 @@
 const getRecipes = async (ingredients) => {
   const apiKey = "efb1a5f145d84a89aa0ba7988e138d65";
-  const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&apiKey=${apiKey}`;
+  const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&apiKey=${apiKey}&number=2`;
 
   try {
     const response = await fetch(url);
