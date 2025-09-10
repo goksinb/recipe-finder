34914900c30c7e0dd66bc8e30de4570c14b3b535
goksinb <gbasok@gmail.com>
2025-09-10 13:47:21 +0200

    add video background

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import Homepage from "./pages/Homepage";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import RecipeDetails from "./pages/recipeDetails";
 
+import About from "./pages/About";
+
 function App() {
   return (
     <BrowserRouter>
       {" "}
       {/* Use BrowserRouter instead of <router> */}
       <Routes>
-        {" "}
-        {/* Use Routes instead of <routes> */}
         <Route path="/" element={<Homepage />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,11 +6,16 @@ function Header() {
   return (
     <header>
       <nav>
-        <Link to="/">
+        {/* Logo goes Home and requests reset */}
+        <Link to="/" state={{reset: true}}>
           <h1>Fridge Feast</h1>
         </Link>
+
         <div className="nav-links">
-          <Link to="/">Homepage</Link>
+          {/* Home link also requests reset */}
+          <Link to="/" state={{reset: true}}>
+            Homepage
+          </Link>
           <Link to="/recipes"> Recipes</Link>
           <Link to="/about"> About</Link>
         </div>
